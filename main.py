558f53e901c08bdeb12281a0cbdf21d76651cfd2
timka <git@timka.su>
2023-11-27 14:10:49 +0300

    allow to edit and create items

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import sys
+import sqlite3
 
 from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget
 from PyQt5 import uic
 
 
@@ -10,6 +11,8 @@ class CoffeeProgram(QMainWindow):
         super().__init__()
         uic.loadUi("main.ui", self)
         self.setup_database()
+        self.createButton.clicked.connect(self.show_add_widget)
+        self.updateButton.clicked.connect(self.update_button)
 
     def setup_database(self):
         self.db = QSqlDatabase.addDatabase('QSQLITE')
@@ -19,6 +22,32 @@ class CoffeeProgram(QMainWindow):
         self.model.setTable('items')
         self.model.select()
         self.items.setModel(self.model)
+
+    def show_add_widget(self):
+        print("test")
+        self.widget = AddCoffeeForm()
+
+    def update_button(self):
+        self.setup_database()
+
+
+class AddCoffeeForm(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("addEditCoffeeForm.ui", self)
+        self.submitButton.clicked.connect(self.create_item)
+        self.show()
+
+    def create_item(self):
+        con = sqlite3.connect("coffee.sqlite")
+        cursor = con.cursor()
+        cursor.execute(f"""INSERT INTO items(sort_name,hot,type,description,price,volume) VALUES (
+            '{self.sortEdit.text()}','{self.hotEdit.text()}','{self.typeEdit.text()}','{self.descriptionEdit.text()}',
+            '{self.priceEdit.text()}','{self.volumeEdit.text()}'
+        )""")
+        con.commit()
+        con.close()
+        self.close()
 
 
 if __name__ == '__main__':
